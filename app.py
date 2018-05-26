2f88ad5a828ecb44418df49cc127bc050e037000
Eniehack <studio4units@gmail.com>
2018-05-27 00:33:09 +0900

    /searchにおいて、データをDBから取得し、jsonを出力できるようにした

--- app.py
+++ app.py
@@ -6,43 +6,123 @@ from tinydb import TinyDB, Query
 db = TinyDB('db.json')
 table = db.table('subway')
 
+Data = Query()
+msg ={}
 
-class Search(Object):
+
+class Search():
 
     def on_get(self, req, resp):
 
         # パラメータを取得
         name = req.get_param('name')
-        type = req.get_param('type')
+        types = req.get_param('type')
 
         # nameパラメータに値が入っているのかを確認
         if name:
-            if type:
+            if types:
                 # name,typeパラメータ共に値が存在するとき
                 # データをDBから取得
+                get_data = table.search((Data.name == name) & (Data.type == types))
                 # for文を使ってひとつづつデータをJSON形式に整形
+                for row in get_data:
+                    msg['name'] = row['name']
+                    msg['name_en'] = row['name_en']
+                    msg['type'] = row['type']
+                    msg['price_regular']= row['price_regular']
+                    msg['price_footlong'] = row['price_footlong']
+                    if row['dressing']:
+                        msg['dressing'] = row['dressing']
+                    msg['calorie'] = row['calorie']
+                    msg['protein'] = row['protein']
+                    msg['lipid'] = row['lipid']
+                    msg['carbohydrate'] = row['carbohydrate']
+                    msg['sodium'] = row['sodium']
+                    msg['saltEquivalent'] = row['saltEquivalent']
+                    msg['detail'] = row['detail']
+                    msg['image_url'] = row['image_url']
+                    # JSON形式でレスポンス
+                    resp.body = json.dumps(msg)
+                    resp.status = falcon.HTTP_200
 
             else:
                 # nameパラメータのみに値が存在するとき
                 # データをDBから取得
+                get_data = table.search(Data.name == name)
                 # for文を使ってひとつづつデータをJSON形式に整形
+                for row in get_data:
+                    msg['name'] = row['name']
+                    msg['name_en'] = row['name_en']
+                    msg['type'] = row['type']
+                    msg['price_regular']= row['price_regular']
+                    msg['price_footlong'] = row['price_footlong']
+                    if row['dressing']:
+                        msg['dressing'] = row['dressing']
+                    msg['calorie'] = row['calorie']
+                    msg['protein'] = row['protein']
+                    msg['lipid'] = row['lipid']
+                    msg['carbohydrate'] = row['carbohydrate']
+                    msg['sodium'] = row['sodium']
+                    msg['saltEquivalent'] = row['saltEquivalent']
+                    msg['detail'] = row['detail']
+                    msg['image_url'] = row['image_url']
+                    # JSON形式でレスポンス
+                    resp.body = json.dumps(msg)
+                    resp.status = falcon.HTTP_200
 
         else:
-            if type:
+            if types:
                 # typeパラメータのみに値が存在するとき
                 # データをDBから取得
+                get_data = table.search(Data.type == types)
                 # for文を使ってひとつづつデータをJSON形式に整形
+                for row in get_data:
+                    msg['name'] = row['name']
+                    msg['name_en'] = row['name_en']
+                    msg['type'] = row['type']
+                    msg['price_regular']= row['price_regular']
+                    msg['price_footlong'] = row['price_footlong']
+                    if row['dressing']:
+                        msg['dressing'] = row['dressing']
+                    msg['calorie'] = row['calorie']
+                    msg['protein'] = row['protein']
+                    msg['lipid'] = row['lipid']
+                    msg['carbohydrate'] = row['carbohydrate']
+                    msg['sodium'] = row['sodium']
+                    msg['saltEquivalent'] = row['saltEquivalent']
+                    msg['detail'] = row['detail']
+                    msg['image_url'] = row['image_url']
+                    # JSON形式でレスポンス
+                    resp.body = json.dumps(msg)
+                    resp.status = falcon.HTTP_200
 
             else:
                 # パラメータが指定されていない場合、全てのエントリを返す
                 # データをDBから取得
                 get_data = table.all()
                 # for文を使ってひとつづつデータをJSON形式に整形
+                for row in get_data:
+                    msg['name'] = row['name']
+                    msg['name_en'] = row['name_en']
+                    msg['type'] = row['type']
+                    msg['price_regular']= row['price_regular']
+                    msg['price_footlong'] = row['price_footlong']
+                    if row['dressing']:
+                        msg['dressing'] = row['dressing']
+                    msg['calorie'] = row['calorie']
+                    msg['protein'] = row['protein']
+                    msg['lipid'] = row['lipid']
+                    msg['carbohydrate'] = row['carbohydrate']
+                    msg['sodium'] = row['sodium']
+                    msg['saltEquivalent'] = row['saltEquivalent']
+                    msg['detail'] = row['detail']
+                    msg['image_url'] = row['image_url']
 
-        # JSON形式でレスポンス
-        #resp.body = json.dumps(msg)
+                    # JSON形式でレスポンス
+                    resp.body = json.dumps(msg)
+                    resp.status = falcon.HTTP_200
 
-
+'''
 class Random(Object):
 
     def on_get(self, req, resp):
@@ -52,10 +132,14 @@ class Random(Object):
         # for文を使ってひとつづつデータをJSON形式に整形
 
         # JSON形式でレスポンス
-        #resp.body = json.dumps(msg)
-
+        resp.body = json.dumps(msg)
+'''
 app = falcon.API()
 app.add_route("/search", Search())
-app.add_route("/random", Random())
+# app.add_route("/random", Random())
 
-#table.insert({'name': , 'type': , 'regular_price': , 'long_price': , 'calorie': , 'protein': , 'lipid': , 'carbohydrate'; , 'sodium'; , 'saltEquivalent': , 'description': , 'imageURL': . 'recommend_dressing': })
+if __name__ == "__main__":
+    from wsgiref import simple_server
+
+    httpd = simple_server.make_server("127.0.0.1", 8008, app)
+    httpd.serve_forever()
